baa73c79d48ad1d21687c125814ef710d0eb3e3b
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-04-14 23:27:11 +0300

    🎨 ui(app): added style variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      inherit: 'inherit',
+      inter: [
+        'inter',
+        'ui-sans-serif',
+        'system-ui',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+      sans: [
+        'Open Sans',
+        'ui-serif',
+        'Georgia',
+        'Cambria',
+        'Times New Roman',
+        'Times',
+        'serif',
+      ],
+    },
+    colors: {
+      white: '#ffffff',
+      gray: colors.gray,
+      blue: colors.blue,
+    },
     extend: {},
   },
   plugins: [],
