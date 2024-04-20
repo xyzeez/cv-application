d37c8d629dcb52bc3a1aaae1fd58283d6ee887b3
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-04-20 07:51:56 +0300

    ✨ feat(app): added progress component

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -3,20 +3,15 @@ import { ArrowDownTrayIcon } from '@heroicons/react/24/outline';
 // Components
 import Button from './button';
 import Resume from './resume';
+import Progress from './progress';
 
 const Preview = ({ relativeStyle }) => {
   return (
     <section className={`${relativeStyle} flex flex-col gap-4`}>
       <div className="flex flex-row items-end justify-between gap-4">
-        <div className="w-full">
+        <div className="w-full flex flex-col gap-1">
           <p>Lorem ipsum dolor sit amet.</p>
-          <div className="w-full bg-gray-200 dark:bg-gray-700">
-            <div
-              className="bg-blue-600 flex items-center justify-center text-xs font-medium text-blue-100 text-center p-0.5 py-1.5 leading-none"
-              style={{ width: '45%' }}>
-              <span>45%</span>
-            </div>
-          </div>
+          <Progress />
         </div>
         <Button Icon={ArrowDownTrayIcon} responsive={true}>
           Download
--- src/components/progress.jsx
+++ src/components/progress.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Progress = () => {
-  return <div>Progress</div>;
+  <progress max="100" value="70"></progress>;
+
+  return (
+    <div className="w-full bg-gray-200 dark:bg-gray-700">
+      <div
+        className="bg-blue-600 flex items-center justify-center text-xs font-medium text-blue-100 text-center p-0.5 py-1.5 leading-none"
+        style={{ width: '45%' }}>
+        <span>45%</span>
+      </div>
+    </div>
+  );
 };
 
 export default Progress;
